83344def1d38acf08e38ddeb4078739aa6ac65cb
Akkio <satriayuda89@gmail.com>
2024-01-10 22:25:09 +0700

    Testing add data

---
+++ script/firebaseConfig.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBOorlBTiyeplZO_r-teScXyT0ZrfL08O8",
+  authDomain: "icikiwir-hackfest2024.firebaseapp.com",
+  projectId: "icikiwir-hackfest2024",
+  storageBucket: "icikiwir-hackfest2024.appspot.com",
+  messagingSenderId: "283109402908",
+  appId: "1:283109402908:web:4d04fde39278314eac72d9",
+  measurementId: "G-MZMJ3KS85J"
+};
+
